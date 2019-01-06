a2574cc984259f9292d093e5368927743fba4be8
Cmoney <cascherer42@gmail.com>
2019-01-06 02:17:27 -0600

    Updates

--- precache-manifest.6f83edf5e1659eac3a31041a98ddea38.js
+++ precache-manifest.6f83edf5e1659eac3a31041a98ddea38.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/React-portfolio/static/js/runtime~main.a2100186.js"
   },
   {
-    "revision": "b7d79f0160d3676763bd",
-    "url": "/React-portfolio/static/js/main.b7d79f01.chunk.js"
+    "revision": "69738faa6d5f281388fc",
+    "url": "/React-portfolio/static/js/main.69738faa.chunk.js"
   },
   {
     "revision": "cf9ab8b4e0ddfb017e78",
     "url": "/React-portfolio/static/js/1.cf9ab8b4.chunk.js"
   },
   {
-    "revision": "b7d79f0160d3676763bd",
+    "revision": "69738faa6d5f281388fc",
     "url": "/React-portfolio/static/css/main.48240a13.chunk.css"
   },
   {
-    "revision": "00f088088520c7a7944559855dd54c70",
+    "revision": "22f29422da4d9c562ea23bd0f1db79bc",
     "url": "/React-portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React-portfolio/precache-manifest.e5890589821818cd0d381c7993f4e776.js"
+  "/React-portfolio/precache-manifest.6f83edf5e1659eac3a31041a98ddea38.js"
 );
 
 workbox.clientsClaim();
