c8a6add12cc6b921eef14a86ecda2009fdd993cf
Cmoney <cascherer42@gmail.com>
2019-01-06 01:29:56 -0600

    Updates

---
+++ precache-manifest.6f3e8c79fd6649b43ede1da59a6f8113.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b3b1d1736f2c5113254",
+    "url": "/React_Portfolio/static/js/runtime~main.0b3b1d17.js"
+  },
+  {
+    "revision": "7bea346f292b5e2be6fc",
+    "url": "/React_Portfolio/static/js/main.7bea346f.chunk.js"
+  },
+  {
+    "revision": "cf9ab8b4e0ddfb017e78",
+    "url": "/React_Portfolio/static/js/1.cf9ab8b4.chunk.js"
+  },
+  {
+    "revision": "7bea346f292b5e2be6fc",
+    "url": "/React_Portfolio/static/css/main.48240a13.chunk.css"
+  },
+  {
+    "revision": "609d09c2ca761f16e2f4b84d4d136519",
+    "url": "/React_Portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Memory_game/precache-manifest.cbb8c894f807336485778186d6a515cd.js"
+  "/React_Portfolio/precache-manifest.6f3e8c79fd6649b43ede1da59a6f8113.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Memory_game/index.html", {
+workbox.routing.registerNavigationRoute("/React_Portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
